7d475a48d11b9daaa587f7d94e041dbdc3ec98ad
EugeneGavrilin <julentver1@gmail.com>
2016-11-11 17:26:14 +0300

    class Person: compare functions, comparator(by name), test plus sample demo

---
+++ src/Person.java
@@ -0,0 +1,75 @@
+import java.util.*;
+
+public class Person implements Comparable<Person> {
+
+    private final String name;
+    private final Date birthday;
+    private final int passport;
+
+    public Person(String name, Date birthday, int passport) {
+        this.name = name;
+        this.birthday = birthday;
+        this.passport = passport;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public int getPassport() {
+        return passport;
+    }
+
+    public int calculateAge() {
+        Calendar dateBirthday = Calendar.getInstance();
+        Calendar today = Calendar.getInstance();
+        dateBirthday.setTime(this.birthday);
+        dateBirthday.add(Calendar.DAY_OF_MONTH, -1);
+        int age = today.get(Calendar.YEAR) - dateBirthday.get(Calendar.YEAR);
+        if (today.get(Calendar.DAY_OF_YEAR) <= dateBirthday.get(Calendar.DAY_OF_YEAR)) {
+            age--;
+        }
+        return age;
+    }
+
+    @Override
+    public int compareTo(Person secondPerson) {
+        if (this.equals(secondPerson)) {
+            return 0;
+        }
+        int comparableValue = calculateAge() - secondPerson.calculateAge();
+        if (comparableValue == 0) {
+            comparableValue = passport - secondPerson.passport;
+        }
+        return comparableValue / Math.abs(comparableValue);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (passport != person.passport) return false;
+        if (!name.equals(person.name)) return false;
+        return birthday.equals(person.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + birthday.hashCode();
+        result = 31 * result + passport;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Name=" + name + ", birthday=" + birthday + ", passport=" + passport;
+    }
+}
---
+++ src/PersonDemo.java
@@ -0,0 +1,80 @@
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class PersonDemo {
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        String[] arrayOfNames = {"Evgenii", "Alex", "Victor", "Elena", "Vlad"};
+        Person[] arrayOfPerson = randomData(arrayOfNames);
+
+        System.out.println("Persons:");
+        printPersons(arrayOfPerson);
+
+        System.out.println("\nSort by age:");
+        sortByAge(arrayOfPerson);
+
+        System.out.println("\nSort by name:");
+        sortByName(arrayOfPerson);
+
+    }
+
+    private static Person[] randomData(String[] arrayOfNames) {
+        Random rnd = ThreadLocalRandom.current();
+        Person[] ret = new Person[5];
+        for (int i = 0; i < 5; i++) {
+            Date date = createDate(1993 + (4 * i / 3), rnd.nextInt(12) + 1, rnd.nextInt(31) + 1);
+            int passport = rnd.nextInt(2000000000);
+            ret[i] = new Person(arrayOfNames[i], date, passport);
+        }
+        return ret;
+    }
+
+    private static void printPersons(Person[] array) {
+        for (Person numberPerson : array) {
+            System.out.println(numberPerson.getName() + ". Age = " + numberPerson.calculateAge());
+        }
+    }
+
+    private static void sortByName(Person[] array) {
+        Arrays.sort(array, new PersonComparator());
+        printPersons(array);
+    }
+
+    private static void sortByAge(Person[] array) {
+        Arrays.sort(array);
+        printPersons(array);
+    }
+
+    private static Date createDate(int year, int month, int day) {
+        checkDay(day);
+        checkMonth(month);
+        checkYear(year);
+        Calendar calendar = GregorianCalendar.getInstance();
+        calendar.set(Calendar.MONTH, month - 1);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        calendar.set(Calendar.YEAR, year);
+        return calendar.getTime();
+    }
+
+    private static void checkDay(int day) throws IllegalArgumentException {
+        if (day > 31 || day < 1) {
+            throw new IllegalArgumentException("Invalid day format");
+        }
+    }
+
+    private static void checkYear(int year) throws IllegalArgumentException {
+        if (year > 2016 || year < 1) {
+            throw new IllegalArgumentException("Invalid year format");
+        }
+    }
+
+    private static void checkMonth(int month) throws IllegalArgumentException {
+        if (month > 12 || month < 1) {
+            throw new IllegalArgumentException("Invalid month format");
+        }
+    }
+}
